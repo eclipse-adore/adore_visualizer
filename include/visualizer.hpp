0f5efbfea3a464e4ff4b62ce140d3a4c64fd7784
Andrew Koerner <andrew.koerner@dlr.de>
2025-09-03 16:07:41 +0200

    normalized eclipse doc headers
    Signed-off-by: Andrew Koerner <andrew.koerner@dlr.de>

--- include/visualizer.hpp
+++ include/visualizer.hpp
@@ -1,16 +1,16 @@
 /********************************************************************************
- * Copyright (C) 2024-2025 German Aerospace Center (DLR).
- * Eclipse ADORe, Automated Driving Open Research https://eclipse.org/adore
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
+ * https://www.eclipse.org/legal/epl-2.0
  *
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *    Marko Mizdrak
  ********************************************************************************/
+
 #pragma once
 #include "adore_dynamics_conversions.hpp"
 #include "adore_map_conversions.hpp"
